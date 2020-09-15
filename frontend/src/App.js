bbd387118be12ded94c1465c63bceea5b3d19324
Brandon Pessman <PESSMABM5831@uwec.edu>
2020-09-14 20:32:31 -0500

    Adds people block ui to meeting creator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import People from './components/People'
+
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import AppBar from '@material-ui/core/AppBar'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Toolbar from '@material-ui/core/Toolbar'
+import List from '@material-ui/core/List'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+
+import MeetingCreatorIcon from '@material-ui/icons/AddBox'
+import UpcomingScheduleIcon from '@material-ui/icons/CalendarToday'
+import PastScheduleIcon from '@material-ui/icons/History'
+import LogoutIcon from '@material-ui/icons/ExitToApp'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerContainer: {
+    overflow: 'auto'
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}))
+
+export default function ClippedDrawer () {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position='fixed' className={classes.appBar}>
+        <Toolbar>
+          <Typography variant='h6' noWrap>
+            Meeting Scheduler
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant='permanent'
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem button key='nav-meeting-creator'>
+              <ListItemIcon>
+                <MeetingCreatorIcon />
+              </ListItemIcon>
+              <ListItemText primary='Meeting Creator' />
+            </ListItem>
+            <ListItem button key='nav-upcoming-schedule'>
+              <ListItemIcon>
+                <UpcomingScheduleIcon />
+              </ListItemIcon>
+              <ListItemText primary='Upcoming Schedule' />
+            </ListItem>
+            <ListItem button key='nav-past-schedule'>
+              <ListItemIcon>
+                <PastScheduleIcon />
+              </ListItemIcon>
+              <ListItemText primary='Past Schedule' />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button key='nav-logout'>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary='Logout' />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <People />
+      </main>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ frontend/src/components/People.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+//import Button from '@material-ui/core/Button'
+import InputBase from '@material-ui/core/InputBase'
+import IconButton from '@material-ui/core/IconButton'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '100%'
+    }
+  }
+}))
+
+export default function SimplePaper () {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <h1 style={{ marginBottom: '0' }}>Meeting Creator</h1>
+      <h2 style={{ marginBottom: '0', marginTop: '0' }}>People</h2>
+      <Paper elevation={3} style={{ padding: '10px 30px' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <h3>Add a Candidate</h3>
+            <span
+              style={{
+                backgroundColor: 'rgba(0,0,0,.1)',
+                padding: '9px',
+                borderRadius: '4px'
+              }}
+            >
+              <InputBase
+                className={classes.input}
+                placeholder='Add a candidate...'
+                inputProps={{ 'aria-label': 'search google maps' }}
+              />
+            </span>
+            <IconButton type='submit' aria-label='search'>
+              <AddCircleIcon style={{ fill: 'blue', marginBottom: '2px' }} />
+            </IconButton>
+          </Grid>
+          <Grid item xs={6}>
+            <h3>Candidates</h3>
+            <p>None</p>
+          </Grid>
+
+          <Grid item xs={6}>
+            <h3>Add a Feedbacker</h3>
+            <span
+              style={{
+                backgroundColor: 'rgba(0,0,0,.1)',
+                padding: '9px',
+                borderRadius: '4px'
+              }}
+            >
+              <InputBase
+                className={classes.input}
+                placeholder='Add a feedbacker...'
+                inputProps={{ 'aria-label': 'search google maps' }}
+              />
+            </span>
+            <IconButton type='submit' aria-label='search'>
+              <AddCircleIcon style={{ fill: 'blue', marginBottom: '2px' }} />
+            </IconButton>
+          </Grid>
+          <Grid item xs={6}>
+            <h3>Feedbackers</h3>
+            <p>None</p>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  )
+}
