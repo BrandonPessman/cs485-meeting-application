2c690a820b324dad82615eda925cc9790d08a636
Brandon Pessman <PESSMABM5831@uwec.edu>
2020-09-28 21:03:41 -0500

    Updates users backend

--- backend/Driver.js
+++ backend/Driver.js
@@ -6,7 +6,7 @@ var email;
 var type;
 class Driver {
   /*Establishes connection to mySQL database - Interview Tracker*/
-  constructor () {
+  constructor() {
     this.connection = MySQL.createConnection({
       host: 'db-mysql-nyc1-50615-do-user-4426317-0.b.db.ondigitalocean.com',
       user: 'doadmin',
@@ -23,21 +23,21 @@ class Driver {
   quit() {
     this.connection.end()
   }
-  getAllMeetings(request, response){
-    const query='SELECT * FROM Meeting';
-    this.connection.query(query,(error,rows)=>{
-        if(error){
-            console.log(error.message);
-        }
-        else{
-            response.send({
-            meetings:rows.map(mapMeetings),
-          });
-        }
+  getAllMeetings(request, response) {
+    const query = 'SELECT * FROM Meeting';
+    this.connection.query(query, (error, rows) => {
+      if (error) {
+        console.log(error.message);
+      }
+      else {
+        response.send({
+          meetings: rows.map(mapMeetings),
+        });
+      }
     });
-}
+  }
   /*Inserts user to 'user' table*/
-  insertUser (id, email, password, phone, name, type) {
+  insertUser(id, email, password, phone, name, type) {
     var query =
       "INSERT INTO user (u_id, email, u_password, phone_number, name, type) VALUES (" +
       id +
@@ -58,17 +58,17 @@ class Driver {
     })
   }
   /*Updates user in 'user' table*/
-  updateUser({id, password, phone, name, type}){
+  updateUser({ id, password, phone, name, type }) {
     var currentUser = getUser(id)
     var update = [password, phone, name, type]
     var original = ['u_password', 'phone_number', 'name', 'type']
-    for (var i = 0; i<update.length; i++) {
+    for (var i = 0; i < update.length; i++) {
       /*Checks if original value in db is same as new, if not updates it*/
       if (currentUser.original[i] === update[i]) {
         var query = 'UPDATE user SET ' + original[i] + ' = ' + update[i] + 'WHERE u_id = ' + id
         return this.connection.query(query, function (err, results) {
-        if (err) throw err
-        console.log(results)
+          if (err) throw err
+          console.log(results)
         })
       }
     }
@@ -82,17 +82,15 @@ class Driver {
     })
   }
   /*Gets all users from 'user' table*/
-  getAllUsers() {
-    const query='SELECT * FROM user';
-    this.connection.query(query,(error,rows)=>{
-        if(error){
-            console.log(error.message);
-        }
-        else{
-            response.send({
-            meetings:rows.map(mapUsers)
-          });
-        }
+  getAllUsers(response) {
+    const query = 'SELECT * FROM user';
+    this.connection.query(query, (error, rows) => {
+      if (error) {
+        console.log(error.message);
+      }
+      else {
+        response.json(rows[0]);
+      }
     });
   }
   /*Gets all users from specific meeting user meeting_id*/
@@ -106,7 +104,7 @@ class Driver {
   /*Returns string of user type from 'userTypes' table*/
   getUserType(id) {
     var query = 'SELECT type_descr FROM userTypes WHERE type_id IN (SELECT type FROM user where u_id = ' + id + ')'
-    return this.connection.query(query, function(err, results) {
+    return this.connection.query(query, function (err, results) {
       if (err) throw err;
       console.log(results)
     })
@@ -114,14 +112,14 @@ class Driver {
   /*Adds each user in meeting to meetingCombo table, only ever called by insertMeeting*/
   meetingCombo(user_id, meeting_id) {
     var query = 'Insert into meetingUser (meeting_id, u_id) VALUES(' + meeting_id + ', ' + user_id + ')'
-      this.connection.query(query, function (err, results) { 
-        if (err) throw err;
-        console.log(results);
-        })
+    this.connection.query(query, function (err, results) {
+      if (err) throw err;
+      console.log(results);
+    })
   }
   /*Inserts a new meeting to 'Meeting' table
     calls meetingCombo to add users to meetingCombo table*/
-  insertMeeting (id, location, users, start_time, end_time) {
+  insertMeeting(id, location, users, start_time, end_time) {
     var query =
       "INSERT INTO Meeting (meeting_id, location_id, users, start_date_time, end_date_time) VALUES (" +
       id +
@@ -134,14 +132,14 @@ class Driver {
       "', '" +
       end_time +
       "')"
-      this.connection.query(query, function (err, results) {
-        if (err) throw err;
-        console.log(results)
-      })
-      /*separates users string into array of user int*/
+    this.connection.query(query, function (err, results) {
+      if (err) throw err;
+      console.log(results)
+    })
+    /*separates users string into array of user int*/
     var users = users.split(",");
     /*For each user in 'users' string call meetingCombo*/
-    for (var i = 0; i<users.length; i++) {
+    for (var i = 0; i < users.length; i++) {
       var user_id = parseInt(users[i]);
       this.meetingCombo(user_id, id);
     }
@@ -161,7 +159,7 @@ class Driver {
     var list = [location, users, start_time, end_time]
     var objectList = [currentMeeting.location_id, currentMeeting.users, currentMeeting.start_time, currentMeeting.end_time]
     /*Checks if original value in column is equal to the new one, if not, update*/
-    for (var i = 0; i<list.length; i++) {
+    for (var i = 0; i < list.length; i++) {
       if (objectList[i] != list[i]) {
         this.connection.query("UPDATE Meeting SET " + objectList[i] + ' = ' + list[i] + ' WHERE meeting_id = ' + id, function (err, results) {
           if (err) throw err;
@@ -179,7 +177,7 @@ class Driver {
     })
   }
   /*Inserts feedback instance into 'Feedback' table*/
-  insertFeedback (id, content, author, meeting_id) {
+  insertFeedback(id, content, author, meeting_id) {
     let now = moment().format("YYYY-MM-DD HH:mm:ss");
     var query =
       "INSERT INTO Feedback (feedback_Id, content, author, date_time_created, meeting_id) VALUES (" +
@@ -193,37 +191,37 @@ class Driver {
       "'," +
       meeting_id +
       ")"
-    var comboQuery = 'INSERT INTO feedbackCombo (feedback_id, meeting_id, author_email) VALUES ('+
-        id +
-        ", " +
-        meeting_id + 
-        ", '" +
-        author + 
-        "')"
-        /*Adds combination to insertFeedbackCombo*/
+    var comboQuery = 'INSERT INTO feedbackCombo (feedback_id, meeting_id, author_email) VALUES (' +
+      id +
+      ", " +
+      meeting_id +
+      ", '" +
+      author +
+      "')"
+    /*Adds combination to insertFeedbackCombo*/
     this.insertFeedbackCombo(comboQuery)
     return this.connection.query(query, function (err, results) {
       if (err) throw err;
       console.log(results)
-      })
+    })
   }
   /*Get all feedback that exist in specific Meeting*/
   getFeedbackMeeting(meetingId) {
     var query = 'SELECT * FROM Feedback WHERE meeting_id = ' + meetingId
     return this.connection.query(query, function (err, results) {
       if (err) throw err
-      console.log(results)      
+      console.log(results)
     })
   }
   /*Returns all feedback from 'Feedback' table*/
   getAllFeedback() {
     var query = 'SELECT * FROM Feedback'
-    return this.connection.query(query, (err, rows)=> {
-      if (err){
+    return this.connection.query(query, (err, rows) => {
+      if (err) {
         console.log(err)
       }
       response.send({
-        feedback:rows.map(mapFeedback)
+        feedback: rows.map(mapFeedback)
       })
     })
   }
@@ -238,13 +236,13 @@ class Driver {
   /*Returns all positions from 'EmployeePosition' table*/
   getPositions() {
     var query = 'SELECT * FROM EmployeePosition';
-    this.connection.query(query, (err, rows) =>{
+    this.connection.query(query, (err, rows) => {
       if (err) {
         console.log(err)
       }
-      else{
+      else {
         response.send({
-          positions:rows.map(mapPosition)
+          positions: rows.map(mapPosition)
         })
       }
     })
@@ -252,13 +250,15 @@ class Driver {
   /*Returns all locations from 'Location' table*/
   getLocations() {
     var query = 'SELECT * FROM Location';
-    this.connection.query(query, (err, rows)=>{
+    this.connection.query(query, (err, rows) => {
       if (err) {
-       console.log(err)
+        console.log(err)
       }
-      else{response.send({
-        locations:rows.map(mapLocation)
-      })}
+      else {
+        response.send({
+          locations: rows.map(mapLocation)
+        })
+      }
     })
   }
   /*Returns all user types from 'userType' table*/
@@ -268,8 +268,9 @@ class Driver {
       if (err) {
         console.log(err)
       }
-      else{response.send({
-        types:rows.map(mapTypes)
+      else {
+        response.send({
+          types: rows.map(mapTypes)
         })
         console.log(rows)
       }
@@ -280,64 +281,64 @@ class Driver {
     date = date.toISOString();
     date = date.substr(1, 19);
     date = date.replace('T', ' ');
-    return(date);
+    return (date);
   }
 }
 /*Maps Meeting columns for response.send() functionality*/
-function mapMeetings(row){
-  return{
-    meeting_id : row.meeting_id,
-    location_id : row.location_id,
-    users : row.users,
-    start_date_time : row.start_date_time,
-    end_date_time : row.end_date_time,
-    meeting_length:row.meeting_length,
-    meeting_status:row.meeting_status,
+function mapMeetings(row) {
+  return {
+    meeting_id: row.meeting_id,
+    location_id: row.location_id,
+    users: row.users,
+    start_date_time: row.start_date_time,
+    end_date_time: row.end_date_time,
+    meeting_length: row.meeting_length,
+    meeting_status: row.meeting_status,
   };
 }
 /*Maps user columns for response.send() functionality*/
 function mapUsers(row) {
-return{
-    u_id : row.u_id,
-    email : row.email,
-    u_password : row.u_password,
-    phone_number : row.phone_number,
-    name : row.name,
-    type : row.type
+  return {
+    u_id: row.u_id,
+    email: row.email,
+    u_password: row.u_password,
+    phone_number: row.phone_number,
+    name: row.name,
+    type: row.type
   };
 }
 /*Maps Feedback columns for response.send() functionality*/
 function mapFeedback(row) {
-  return{
-    feedback_Id : row.feedback_Id,
-    content : row.content,
-    author : row.author,
-    date_time_created : row.date_time_created,
-    meeting_id : row.meeting_id
+  return {
+    feedback_Id: row.feedback_Id,
+    content: row.content,
+    author: row.author,
+    date_time_created: row.date_time_created,
+    meeting_id: row.meeting_id
   };
 }
 /*Maps Location columns for response.send() functionality*/
 function mapLocation(row) {
-  return{
-  location_id : row.location_id,
-  name : row.name,
-  available : row.available
+  return {
+    location_id: row.location_id,
+    name: row.name,
+    available: row.available
   };
 }
 /*Maps Position columns for response.send() functionality*/
 function mapPosition(row) {
-  return{
-  position_id : row.position_id,
-  title : row.title,
-  currentEmployee : row.currentEmployee
+  return {
+    position_id: row.position_id,
+    title: row.title,
+    currentEmployee: row.currentEmployee
   };
 }
 /*Maps userTypes columns for response.send() functionality*/
 function mapTypes(row) {
-  return{
+  return {
     type_id: row.type_id,
     type_descr: row.type_descr
   };
 }
 var newdriver = new Driver();
-exports.newdriver=newdriver;
+exports.newdriver = newdriver;
--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,38 @@
-var express=require('express');
-var app=express();
-var cors=require('cors');
-const driver = require("./driver.js"); 
+var express = require('express');
+var app = express();
+var cors = require('cors');
+const driver = require("./driver.js");
 
 app.use(cors());
 
-const port=3443;
-app.listen(port,()=>{
-   console.log(`Live on port ${port}`);
+const port = 3443;
+app.listen(port, () => {
+    console.log(`Live on port ${port}`);
 });
-app.get('/types', (request,response) => {
+app.get('/types', (request, response) => {
     driver.newdriver.getUserTypes(request, response);
 });
-app.get('/meetings',(request, response) =>{
+app.get('/meetings', (request, response) => {
     driver.newdriver.getAllMeetings(request, response);
 });
-app.get('/users',(request, response) =>{
-    response.send(driver.newdriver.getAllUsers());
+app.get('/users', (request, response) => {
+    driver.newdriver.getAllUsers(response);
 });
 app.get('/users:u_id', (request, response) => {
     const u_id = request.params.u_id;
     response.send(driver.newdriver.getUser(u_id));
 })
 app.get('/positions', (request, response) => {
-    driver.newdriver.getPositions(request,response);
+    driver.newdriver.getPositions(request, response);
 })
 app.get('/locations', (request, response) => {
-    response.send(driver.newdriver.getLocations()); 
+    response.send(driver.newdriver.getLocations());
 })
 app.get('/feedback:meeting_id', (request, response) => {
     const meeting_id = request.params.meeting_id;
     response.send(driver.newdriver.getFeedbackMeeting(meeting_id));
 })
-app.get('/user:meeting_id', (request, response) =>{
+app.get('/user:meeting_id', (request, response) => {
     const meeting_id = request.params.meeting_id;
     response.send(driver.newdriver.getMeeetingUsers);
 })
