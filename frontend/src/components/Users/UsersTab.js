83765571093f900462e95e6b1d79ed5eb8f0e98c
Brandon Pessman <PESSMABM5831@uwec.edu>
2020-10-03 03:35:12 -0500

    Test

--- frontend/src/components/Users/UsersTab.js
+++ frontend/src/components/Users/UsersTab.js
@@ -7,6 +7,7 @@ export default function Department({ setShowNextStep }) {
     <div>
       <Grid container spacing={12} style={{ marginTop: '10px' }}>
         <UsersTable />
+        <p>Test</p>
       </Grid>
     </div>
   )
